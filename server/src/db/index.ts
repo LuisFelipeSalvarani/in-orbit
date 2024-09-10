5a71688884f9fadd7d964439ab08574d1f36e25d
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-09-10 09:58:42 -0300

    Criação de dados no banco

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,25 +1,7 @@
-import { createId } from '@paralleldrive/cuid2'
-import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+import * as schema from './schema'
+import { env } from '../env'
 
-export const goals = pgTable('goals', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  title: text('title').notNull(),
-  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
-  createdAt: timestamp('created_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-})
-
-export const goalsCompletions = pgTable('goals_completions', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  goalsId: text('goal_id')
-    .references(() => goals.id)
-    .notNull(),
-  createdAt: timestamp('created_at', { withTimezone: true })
-    .notNull()
-    .defaultNow(),
-})
+export const client = postgres(env.DATABASE_URL)
+export const db = drizzle(client, { schema, logger: true })
