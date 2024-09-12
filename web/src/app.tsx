c4ec3c966ace440d1ef3766b4174cb8275a40172
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-09-11 22:10:01 -0300

    Criação do componente de Sumário

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,11 +1,14 @@
 import { CreateGoal } from './components/create-goal'
-import { EmptyGoals } from './components/empty-goal'
+import { Sumarry } from './components/summary'
+// import { EmptyGoals } from './components/empty-goal'
 import { Dialog } from './components/ui/dialog'
 
 export function App() {
   return (
     <Dialog>
-      <EmptyGoals />
+      {/* <EmptyGoals /> */}
+
+      <Sumarry />
 
       <CreateGoal />
     </Dialog>
