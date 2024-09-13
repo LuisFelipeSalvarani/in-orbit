92a007746900f64782c94f5323dd2d921a5efed3
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-09-13 20:08:43 -0300

    Refatoração de fetch e useEffect para react-query e fetch

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,38 +1,19 @@
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-goal'
 import { Sumarry } from './components/summary'
 import { Dialog } from './components/ui/dialog'
-
-type SummaryResponse = {
-  completed: number
-  total: number
-  goalsPerDay: Record<
-    string,
-    {
-      id: string
-      title: string
-      completedAt: string
-    }[]
-  >
-}
+import { getSummary } from './http/get-summary'
 
 export function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null>(null)
-
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setSummary(data.summary)
-      })
-  }, [])
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
 
   return (
     <Dialog>
-      {summary?.total && summary.total > 0 ? <Sumarry /> : <EmptyGoals />}
+      {data?.total && data.total > 0 ? <Sumarry /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
