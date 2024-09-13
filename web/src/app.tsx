90de7f7cfae11ff2a0388b6703f001432ce7427f
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-09-13 19:59:38 -0300

    Busca de dados com fetch

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from 'react'
 import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goal'
 import { Sumarry } from './components/summary'
-// import { EmptyGoals } from './components/empty-goal'
 import { Dialog } from './components/ui/dialog'
 
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
+
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setSummary(data.summary)
+      })
+  }, [])
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-
-      <Sumarry />
+      {summary?.total && summary.total > 0 ? <Sumarry /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
